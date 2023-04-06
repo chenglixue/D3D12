c5156c3c611088262f3ae5d022291aaf14ece5f4
elysia <979130386@qq.com>
2023-04-06 22:31:46 +0800

    2d图形

---
+++ MyDemo/MyDemo/Main.cpp
@@ -0,0 +1,56 @@
+
+#include "stdafx.h"
+#include "MyD3D12.h"
+
+
+//------------------------------------------------------------------------------------------------------------------
+// function declaration
+static std::wstring GetLatestWinPixGpuCapturerPath_Cpp17();
+
+
+//------------------------------------------------------------------------------------------------------------------
+// main function
+
+_Use_decl_annotations_
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    PIXLoadLatestWinPixGpuCapturerLibrary();
+    MyD3D12 sample(1280, 720, L"To romantic unfailing Elysia");
+    return Win32Application::Run(&sample, hInstance, nCmdShow);
+}
+
+
+//------------------------------------------------------------------------------------------------------------------
+// 
+// function definition
+// 
+
+//find path to WinPixGpuCapturer.dll from the most-recently installed version of PIX
+static std::wstring GetLatestWinPixGpuCapturerPath_Cpp17()
+{
+    LPWSTR programFilesPath = nullptr;
+    SHGetKnownFolderPath(FOLDERID_ProgramFiles, KF_FLAG_DEFAULT, NULL, &programFilesPath);
+
+    std::filesystem::path pixInstallationPath = programFilesPath;
+    pixInstallationPath /= "Microsoft PIX";
+
+    std::wstring newestVersionFound;
+
+    for (auto const& directory_entry : std::filesystem::directory_iterator(pixInstallationPath))
+    {
+        if (directory_entry.is_directory())
+        {
+            if (newestVersionFound.empty() || newestVersionFound < directory_entry.path().filename().c_str())
+            {
+                newestVersionFound = directory_entry.path().filename().c_str();
+            }
+        }
+    }
+
+    if (newestVersionFound.empty())
+    {
+        // TODO: Error, no PIX installation found
+    }
+
+    return pixInstallationPath / newestVersionFound / L"WinPixGpuCapturer.dll";
+}
---
+++ MyDemo/MyDemo/stdafx.h
@@ -0,0 +1,33 @@
+/*  
+	Precompiled header files
+	stdafx.h : include file for standard system include files,
+	or project specific include files that are used frequently, 
+	but are changed infrequently
+*/
+
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
+#endif
+
+#include <windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include "d3dx12.h"
+
+#include <string>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <wrl.h>
+#include <shellapi.h>
+#include <cstdint>
+
+//shader debug
+#include "pix3.h"
+#include <filesystem>
+#include <shlobj.h>
